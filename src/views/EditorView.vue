a36c8e23adcb679a8f6b75dee145f5c0ec27f147
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2026-02-24 17:58:50 -0300

    feat: refactor EditorView component to include sidebar, header, and theme selector

--- src/views/EditorView.vue
+++ src/views/EditorView.vue
@@ -1,7 +1,34 @@
-<script lang="ts" setup></script>
+<script setup lang="ts">
+import EditorSidebar from "@/components/editor/EditorSidebar.vue";
+import EditorExamples from "@/components/editor/EditorExamples.vue";
+import EditorHeader from "@/components/editor/EditorHeader.vue";
+import EditorThemeSelector from "@/components/editor/EditorThemeSelector.vue";
+import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { useTheme } from "@/composables/useTheme";
+
+const { theme, themeId, setTheme } = useTheme();
+</script>
 
 <template>
-  <h1>Editor</h1>
-</template>
+  <div class="flex flex-col h-screen [--header-height:44px]">
+    <EditorHeader class="h-(--header-height) shrink-0 border-b" />
+    <SidebarProvider class="flex-1! min-h-0! overflow-hidden! [--sidebar-width:17rem]!">
+      <SidebarInset class="flex flex-col min-h-0! overflow-hidden min-w-0!">
+        <header class="flex justify-between h-14 items-center gap-2 px-4 border-b shrink-0">
+          <EditorThemeSelector
+            class="relative"
+            :current-theme-name="theme.name"
+            :current-theme-id="themeId"
+            @select="(id) => setTheme(id)"
+          />
+          <SidebarTrigger />
+        </header>
 
-<style lang="postcss" scoped></style>
+        <main class="flex-1 overflow-y-hidden p-4">
+          <EditorExamples />
+        </main>
+      </SidebarInset>
+      <EditorSidebar class="top-(--header-height)! h-[calc(100svh-var(--header-height))]!" />
+    </SidebarProvider>
+  </div>
+</template>
