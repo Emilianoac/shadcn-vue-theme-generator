4db880e2728d26d7ff4be32b7a60a2ad111a80fa
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2026-03-02 23:15:11 -0300

    feat(ExampleCard): add new component for displaying examples with clipboard functionality

---
+++ src/data/examples/ExampleCard.vue
@@ -0,0 +1,43 @@
+<script lang="ts" setup>
+import { useClipboard } from "@vueuse/core";
+import { Copy, Check } from "lucide-vue-next";
+import { Button } from "@/components/ui/button";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+
+const props = defineProps<{
+  rawComponent: string;
+}>();
+
+const { copy, copied } = useClipboard({
+  source: props.rawComponent,
+});
+</script>
+
+<template>
+  <div class="space-y-2">
+    <div class="flex justify-between items-center">
+      <span class="block text-sm text-muted-foreground">
+        <slot name="title" />
+      </span>
+
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger as-child>
+            <Button @click="copy()" variant="ghost" size="icon-sm">
+              <Copy v-if="!copied" />
+              <Check v-else />
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Copy to clipboard</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+    </div>
+    <div class="border border-dashed p-6">
+      <slot name="example" />
+    </div>
+  </div>
+</template>
+
+<style lang="postcss" scoped></style>
