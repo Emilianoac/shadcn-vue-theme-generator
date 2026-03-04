c073c383b947d583dd6d671cfc60677036ad4170
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2026-03-04 12:10:26 -0300

    feat(editor-sidebar): wrap SidebarTabs component with ScrollArea for improved layout

--- src/components/editor/EditorSidebar.vue
+++ src/components/editor/EditorSidebar.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import { Sidebar } from "@/components/ui/sidebar";
 import SidebarTabs from "@/components/editor/sidebar/SidebarTabs.vue";
+import { ScrollArea } from "@/components/ui/scroll-area";
 </script>
 
 <template>
@@ -8,7 +9,9 @@ import SidebarTabs from "@/components/editor/sidebar/SidebarTabs.vue";
     <div class="relative flex h-full flex-1 flex-col bg-background">
       <div class="flex min-h-0 w-full flex-1 flex-col">
         <div class="flex min-h-0 flex-1 flex-col space-y-4 mb-3">
-          <SidebarTabs />
+          <ScrollArea class=" ">
+            <SidebarTabs />
+          </ScrollArea>
         </div>
       </div>
     </div>
