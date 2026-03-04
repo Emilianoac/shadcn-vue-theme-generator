db7c2e56ba85d233c7f3108060bb5fbcd7f38ba3
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2026-03-04 17:01:31 -0300

    feat(main): integrate highlight.js for syntax highlighting support

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
+import "highlight.js/styles/github-dark.css";
+import hljs from "highlight.js/lib/core";
+import css from "highlight.js/lib/languages/css";
+import hljsVuePlugin from "@highlightjs/vue-plugin";
 import "@/assets/css/styles.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+hljs.registerLanguage("css", css);
 const app = createApp(App);
 
+app.use(hljsVuePlugin);
 app.use(router);
 
 app.mount("#app");
