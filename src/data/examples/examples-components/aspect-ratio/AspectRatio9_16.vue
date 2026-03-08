45f83ed9b7b730f3ddce000a21c086b6759787ab
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2026-03-08 16:44:23 -0300

    feat(aspect-ratio): add data-component-x-ray attributes for enable  x-ray functionality

--- src/data/examples/examples-components/aspect-ratio/AspectRatio9_16.vue
+++ src/data/examples/examples-components/aspect-ratio/AspectRatio9_16.vue
@@ -5,7 +5,7 @@ import ExampleImage from "@/assets/images/example-image.jpg";
 
 <template>
   <div class="max-w-150 mx-auto">
-    <AspectRatio :ratio="9 / 16" class="bg-muted rounded-lg">
+    <AspectRatio :ratio="9 / 16" class="bg-muted rounded-lg" data-component-x-ray="AspectRatio">
       <img
         :src="ExampleImage"
         alt="A shiba inu sitting in a field of grass"
