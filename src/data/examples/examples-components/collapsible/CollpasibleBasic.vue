4d2ec3bd3a1417861c6be42525573156eb7c2f88
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2026-03-17 13:43:54 -0300

    feat(collapsible examples): add new examples

---
+++ src/data/examples/examples-components/collapsible/CollpasibleBasic.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ChevronDownIcon } from "lucide-vue-next";
+import { Card, CardContent } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@/components/ui/collapsible";
+</script>
+
+<template>
+  <Card class="mx-auto w-full max-w-sm" data-component-x-ray="Card">
+    <CardContent data-component-x-ray="CardContent">
+      <Collapsible class="rounded-md data-[state=open]:bg-muted" data-component-x-ray="Collapsible">
+        <CollapsibleTrigger asChild>
+          <Button variant="ghost" class="group w-full" data-component-x-ray-trigger="Button">
+            Product details
+            <ChevronDownIcon class="ml-auto group-data-[state=open]:rotate-180" />
+          </Button>
+        </CollapsibleTrigger>
+        <CollapsibleContent
+          class="flex flex-col items-start gap-2 p-2.5 pt-0 text-sm"
+          data-component-x-ray="CollapsibleContent"
+        >
+          <div>This panel can be expanded or collapsed to reveal additional content.</div>
+          <Button size="sm">Learn More</Button>
+        </CollapsibleContent>
+      </Collapsible>
+    </CardContent>
+  </Card>
+</template>
---
+++ src/data/examples/examples-components/collapsible/_Examples.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+import ExampleCard from "../../ExampleCard.vue";
+import CollaposibleBasic from "./CollpasibleBasic.vue";
+import CollaposibleBasicRaw from "./CollpasibleBasic.vue?raw";
+import CollapsibleSetting from "./CollapsibleSetting.vue";
+import CollapsibleSettingRaw from "./CollapsibleSetting.vue?raw";
+import CollapsibleFileTree from "./CollapasibleFileTree.vue";
+import CollapsibleFileTreeRaw from "./CollapasibleFileTree.vue?raw";
+</script>
+
+<template>
+  <div class="@container w-250 mx-auto">
+    <div class="grid grid-cols-1 @xl:grid-cols-2 gap-8 mx-auto">
+      <ExampleCard :raw-component="CollaposibleBasicRaw">
+        <template #title> Basic </template>
+        <template #example>
+          <CollaposibleBasic />
+        </template>
+      </ExampleCard>
+      <ExampleCard :raw-component="CollapsibleSettingRaw">
+        <template #title>Setting</template>
+        <template #example>
+          <CollapsibleSetting />
+        </template>
+      </ExampleCard>
+      <ExampleCard :raw-component="CollapsibleFileTreeRaw">
+        <template #title>File Tree</template>
+        <template #example>
+          <CollapsibleFileTree />
+        </template>
+      </ExampleCard>
+    </div>
+  </div>
+</template>
+
+<style lang="postcss" scoped></style>
