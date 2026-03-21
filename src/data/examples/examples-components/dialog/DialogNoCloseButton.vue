7e789c083fc21d144a665884d4c98fb1274165bd
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2026-03-21 14:35:04 -0300

    feat(Dialog examples): refactor dialog examples

---
+++ src/data/examples/examples-components/dialog/DialogNoCloseButton.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+</script>
+
+<template>
+  <Dialog data-component-x-ray="Dialog">
+    <DialogTrigger :as-child="true">
+      <Button variant="outline" data-component-x-ray-trigger="Button"> No Close Button </Button>
+    </DialogTrigger>
+    <DialogContent
+      :show-close-button="false"
+      to="#examples-container"
+      data-component-x-ray="DialogContent"
+    >
+      <DialogHeader data-component-x-ray="DialogHeader">
+        <DialogTitle data-component-x-ray="DialogTitle">No Close Button</DialogTitle>
+        <DialogDescription data-component-x-ray="DialogDescription">
+          This dialog doesn't have a close button in the top-right corner.
+        </DialogDescription>
+      </DialogHeader>
+      <DialogFooter data-component-x-ray="DialogFooter">
+        <DialogClose :as-child="true">
+          <Button variant="outline" data-component-x-ray-trigger="Button"> Close </Button>
+        </DialogClose>
+      </DialogFooter>
+    </DialogContent>
+  </Dialog>
+</template>
---
+++ src/data/examples/examples-components/dialog/_Examples.vue
@@ -0,0 +1,44 @@
+<script lang="ts" setup>
+import ExampleCard from "../../ExampleCard.vue";
+import DialogWithForm from "./DialogWithForm.vue";
+import DialogWithFormRaw from "./DialogWithForm.vue?raw";
+import DialogScrollableContent from "./DialogScrollableContent.vue";
+import DialogScrollableContentRaw from "./DialogScrollableContent.vue?raw";
+import DialogWithStickyFooter from "./DialogWithStickyFooter.vue";
+import DialogWithStickyFooterRaw from "./DialogWithStickyFooter.vue?raw";
+import DialogNoCloseButton from "./DialogNoCloseButton.vue";
+import DialogNoCloseButtonRaw from "./DialogNoCloseButton.vue?raw";
+</script>
+
+<template>
+  <div class="@container max-w-200 mx-auto">
+    <div class="grid grid-cols-1 @xl:grid-cols-2 gap-8 mx-auto">
+      <ExampleCard :raw-component="DialogWithFormRaw">
+        <template #title> With Form </template>
+        <template #example>
+          <DialogWithForm />
+        </template>
+      </ExampleCard>
+      <ExampleCard :raw-component="DialogScrollableContentRaw">
+        <template #title> Scrollable Content </template>
+        <template #example>
+          <DialogScrollableContent />
+        </template>
+      </ExampleCard>
+      <ExampleCard :raw-component="DialogWithStickyFooterRaw">
+        <template #title> Sticky Footer </template>
+        <template #example>
+          <DialogWithStickyFooter />
+        </template>
+      </ExampleCard>
+      <ExampleCard :raw-component="DialogNoCloseButtonRaw">
+        <template #title> No Close Button </template>
+        <template #example>
+          <DialogNoCloseButton />
+        </template>
+      </ExampleCard>
+    </div>
+  </div>
+</template>
+
+<style lang="postcss" scoped></style>
