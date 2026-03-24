9ebe9d168b4af79c6912b5e452381a964959f57d
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2026-03-24 13:13:11 -0300

    feat(Pagination Examples): Improve component examples

--- src/data/examples/examples-components/index.ts
+++ src/data/examples/examples-components/index.ts
@@ -31,7 +31,7 @@ export { default as Menubar } from "./menubar/_Examples.vue";
 export { default as NativeSelect } from "./native-select/_Examples.vue";
 export { default as NavigationMenu } from "./navigation-menu/_Examples.vue";
 export { default as NumberField } from "./number-field/_Examples.vue";
-export { default as Pagination } from "./ComponentPagination.vue";
+export { default as Pagination } from "./pagination/_Examples.vue";
 export { default as Popover } from "./ComponentPopover.vue";
 export { default as Progress } from "./ComponentProgress.vue";
 export { default as RadioGroup } from "./ComponentRadioGroup.vue";
---
+++ src/data/examples/examples-components/pagination/_Examples.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+import ExampleCard from "../../ExampleCard.vue";
+import PaginationBasic from "./PaginationBasic.vue";
+import PaginationBasicRaw from "./PaginationBasic.vue?raw";
+import PaginationSimple from "./PaginationSimple.vue";
+import PaginationSimpleRaw from "./PaginationSimple.vue?raw";
+</script>
+
+<template>
+  <div class="@container max-w-250 mx-auto">
+    <div class="grid grid-cols-1 @xl:grid-cols-2 gap-8 mx-auto">
+      <ExampleCard :raw-component="PaginationBasicRaw">
+        <template #title> Basic </template>
+        <template #example>
+          <PaginationBasic />
+        </template>
+      </ExampleCard>
+      <ExampleCard :raw-component="PaginationSimpleRaw">
+        <template #title> Simple </template>
+        <template #example>
+          <PaginationSimple />
+        </template>
+      </ExampleCard>
+    </div>
+  </div>
+</template>
+
+<style lang="postcss" scoped></style>
