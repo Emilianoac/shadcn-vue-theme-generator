8bea8d7c04975cc145efe16092247a469973c407
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2026-03-28 01:36:23 -0300

    fix(Stats): add container query support for responsive grid layout

--- src/data/examples/examples-screens/cards/components/Stats.vue
+++ src/data/examples/examples-screens/cards/components/Stats.vue
@@ -46,7 +46,7 @@ const chartConfig = {
 </script>
 
 <template>
-  <div class="grid gap-4 grid-cols-1 xl:grid-cols-2">
+  <div class="grid gap-4 grid-cols-1 @xl:grid-cols-2">
     <Card>
       <CardHeader>
         <CardDescription>Total Revenue</CardDescription>
