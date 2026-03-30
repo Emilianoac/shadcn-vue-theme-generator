2869676809b20f03ef61b88ad97c85434873cf8c
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2026-03-30 10:11:09 -0300

    feat(SidebarTabs): Update tab icons and improve layout styles

--- src/components/editor/sidebar/SidebarTabs.vue
+++ src/components/editor/sidebar/SidebarTabs.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import { useEditorBridge } from "@/bridge/UseEditorBridge";
+import { PaletteIcon, BotIcon } from "lucide-vue-next";
+import { useEditorBridge } from "@/bridge/useEditorBridge";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import ControlsColor from "@/components/editor/sidebar/controls/ControlsColor.vue";
 import AutoGenerate from "@/components/editor/sidebar/tabs/auto-generate/AutoGenerate.vue";
@@ -12,11 +13,13 @@ const tabs = [
     id: "colors",
     label: "Colors",
     component: ControlsColor,
+    icon: PaletteIcon,
   },
   {
     id: "auto-generate",
     label: "Auto Generate",
     component: AutoGenerate,
+    icon: BotIcon,
   },
 ];
 
@@ -28,14 +31,21 @@ useEditorBridge({
 </script>
 
 <template>
-  <Tabs v-model="activeTab" class="flex min-h-0 w-full flex-1 flex-col" :unmount-on-hide="false">
-    <TabsList class="bg-transparent p-4 justify-start border-b rounded-none h-14">
+  <Tabs
+    v-model="activeTab"
+    class="flex min-h-0 w-full flex-1 flex-col gap-0"
+    :unmount-on-hide="false"
+  >
+    <TabsList
+      class="d-block space-x-2 bg-background p-3 justify-start rounded-none h-14 sticky top-0 z-10 border-b w-full"
+    >
       <TabsTrigger
         v-for="tab in tabs"
         :key="tab.id"
-        class="py-1 font-semibold rounded-[5px] text-[0.8em] data-[state=active]:bg-secondary data-[state=active]:text-secondary-foreground cursor-pointer data-[state=inactive]:hover:opacity-60"
+        class="py-1 text-start font-semibold rounded-[5px] text-[0.8em] data-[state=active]:bg-secondary data-[state=active]:text-secondary-foreground cursor-pointer data-[state=inactive]:hover:opacity-60"
         :value="tab.id"
       >
+        <component :is="tab.icon" />
         {{ tab.label }}
       </TabsTrigger>
     </TabsList>
