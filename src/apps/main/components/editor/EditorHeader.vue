e10b4a9191a9c00bf11b1f665060f547d89a9f27
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2026-04-02 17:26:29 -0300

    feat: add GitHub repository link to header

--- src/apps/main/components/editor/EditorHeader.vue
+++ src/apps/main/components/editor/EditorHeader.vue
@@ -1,8 +1,17 @@
 <script lang="ts" setup>
 import { useDark } from "@vueuse/core";
 import { Moon, Sun } from "lucide-vue-next";
+import { GithubIcon } from "@/shared/assets/icons";
 
 import { Switch } from "@/shared/components/ui/switch";
+import { Button } from "@/shared/components/ui/button";
+import { Separator } from "@/shared/components/ui/separator";
+import {
+  TooltipProvider,
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+} from "@/shared/components/ui/tooltip";
 import siteLogo from "@/apps/main/components/assets/siteLogo.vue";
 
 const isDark = useDark({
@@ -15,8 +24,25 @@ const isDark = useDark({
 <template>
   <header class="border-b px-4 py-2 flex justify-between items-center">
     <siteLogo class="max-w-47.5 md:max-w-50" />
-    <div class="flex items-center space-x-2">
-      <Switch id="color-schema" v-model="isDark" class="border-border">
+    <div class="flex h-5 items-center space-x-2">
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger as-child>
+            <Button variant="ghost" size="icon-sm" as-child>
+              <a
+                href="https://github.com/Emilianoac/shadcn-vue-theme-generator"
+                target="_blank"
+                aria-label="GitHub Repository"
+              >
+                <GithubIcon class="w-5 h-5" />
+              </a>
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent> GitHub Repository </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+      <Separator orientation="vertical" />
+      <Switch id="color-schema" v-model="isDark" class="border-border cursor-pointer">
         <template #thumb>
           <div class="w-full h-full rounded-full flex items-center justify-center">
             <Moon :size="14" v-if="isDark" />
